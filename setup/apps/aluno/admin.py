3454a5fe32adeae6fcf87fb4b88d878cd1bb9fb6
JoaoVictorSou <soulima.joao@gmail.com>
2022-07-29 08:43:41 -0300

    configuração do admin

--- setup/apps/aluno/admin.py
+++ setup/apps/aluno/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Aluno
 # Register your models here.
+
+class Alunos(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'nome',
+        'rg',
+        'cpf',
+        'nascimento',
+    )
+    list_display_links = (
+        'id',
+        'nome',
+    )
+    search_fields = ( 'nome', )
+    list_per_page = 20
+
+admin.site.register(Aluno, Alunos)
--- setup/apps/curso/admin.py
+++ setup/apps/curso/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Curso
 # Register your models here.
+
+class Cursos(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'codigo_curso',
+        'descricao',
+        'nivel',
+    )
+    list_display_links = (
+        'id',
+        'codigo_curso',
+        'descricao',
+    )
+    search_fields = (
+        'codigo_curso',
+        'descricao',
+    )
+
+admin.site.register(Curso, Cursos)
